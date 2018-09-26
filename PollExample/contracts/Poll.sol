500183816144caf178c2613410d5025a8f0359a2
Bull Market Brokers <hfdurante@gmail.com>
2018-09-25 21:56:07 -0300

    Smart contract compiling first approach

--- PollExample/contracts/Poll.sol
+++ PollExample/contracts/Poll.sol
@@ -1,9 +1,70 @@
 pragma solidity ^0.4.23;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Poll is Ownable {
-  mapping(address => bool) votes;
-  uint positiveVotes;
-  uint negativeVotes;
+
+    using SafeMath for uint16;
+
+    struct VoteStruct {
+        address voter;
+        bool option;
+    }
+
+    VoteStruct[] private votes;
+    uint voteFee = 10 finney;
+    
+    modifier voted() {
+        require(adressVoted());
+        _;
+    }
+
+    modifier pendingVote() {
+        require(!adressVoted());
+        _;
+    }
+
+    function setVoteFee(uint _fee) external onlyOwner  {
+        voteFee = _fee;
+    }  
+
+    function vote(bool _option) external payable pendingVote {
+        require(msg.value >= voteFee, "Ether sent is not enough to vote");
+        VoteStruct memory ballot = VoteStruct(msg.sender, _option);
+        votes.push(ballot);
+    }  
+    
+    function getPollResults() public view voted returns(uint16 positiveVotes,uint16 negativeVotes) {
+        
+        positiveVotes = 0;
+        negativeVotes = 0;
+        uint32 index = 0;
+        while(index < votes.length)
+        {
+            if(votes[index].option) {
+                positiveVotes.add(1); 
+            } else {
+                negativeVotes.add(1);
+            }
+        }
+        //return (positiveVotes,negativeVotes);
+    }
+
+    function adressVoted() public view returns(bool)
+    {
+        bool result = false;
+        if(votes.length > 0)
+        {       
+            uint index = 0;
+            while(index < votes.length || result)
+            {
+                if(votes[index].voter == msg.sender)
+                {
+                    result = true;
+                }
+            }
+         }  
+        return result;
+    }
 
 }
